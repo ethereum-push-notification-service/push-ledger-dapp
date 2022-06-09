721fb34464d17953d0fe0a58566067c6188f300f
Subhranshu Das <subhranshu@Subhranshus-MacBook-Pro.local>
2022-06-09 18:05:22 +0530

    Updates

---
+++ precache-manifest.78dff7cdff8b5bd346f7fd31de16d301.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2accadd1365f56eb4a5bc196901ddefe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b18d61a29cdd0c5d1787",
+    "url": "/static/css/2.5110b71c.chunk.css"
+  },
+  {
+    "revision": "4bab2578a8614a73c2ce",
+    "url": "/static/css/main.9aab321d.chunk.css"
+  },
+  {
+    "revision": "b18d61a29cdd0c5d1787",
+    "url": "/static/js/2.08832f86.chunk.js"
+  },
+  {
+    "revision": "4ff78cfa411395f40a098efea045f2bc",
+    "url": "/static/js/2.08832f86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6101dafbdb7edf96f943",
+    "url": "/static/js/3.ccebb76f.chunk.js"
+  },
+  {
+    "revision": "c388c844464fd931f9a8",
+    "url": "/static/js/4.a8b9f1fd.chunk.js"
+  },
+  {
+    "revision": "0f9d4321b5507e59f5bf",
+    "url": "/static/js/5.f7efb971.chunk.js"
+  },
+  {
+    "revision": "c178649ae0de162657ef6caa733cc024",
+    "url": "/static/js/5.f7efb971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aa820f509962d1e6c73",
+    "url": "/static/js/6.ba859574.chunk.js"
+  },
+  {
+    "revision": "4bab2578a8614a73c2ce",
+    "url": "/static/js/main.e7d3f20f.chunk.js"
+  },
+  {
+    "revision": "d382feeb57bb493bc1aa",
+    "url": "/static/js/runtime-main.af737ea7.js"
+  }
+]);
---
+++ static/js/6.ba859574.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@project/react-app"]=this["webpackJsonp@project/react-app"]||[]).push([[6],{2462:function(p,c){}}]);
+//# sourceMappingURL=6.ba859574.chunk.js.map
